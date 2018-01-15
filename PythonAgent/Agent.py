f0c4e76a8cd94826c8de4fc8dbdd500e7d75de8f
Ghostranger <kiranr0019@gmail.com>
2018-01-16 00:20:56 +0530

    Initializing the Agent.py and Readme for Agent Module/folder
    Agent.py will integate the three submodule training,ConvolutionalNetwork,RacingLine

---
+++ PythonAgent/Agent.py
@@ -0,0 +1,11 @@
+#****************************************************#
+#		Agent Version 1.0
+#
+#	This is the main File to be executed for
+#	training and running the agent and conf-
+#	iguring agent.
+#
+#****************************************************#
+
+import os
+
